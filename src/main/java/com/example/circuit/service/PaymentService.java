7c16372d37104921af137f1941b176fddb6bb678
chaelink <lin5476@naver.com>
2025-12-19 14:33:48 +0900

    Change payApi Random Value

--- src/main/java/com/example/circuit/service/PaymentService.java
+++ src/main/java/com/example/circuit/service/PaymentService.java
@@ -10,13 +10,13 @@ public class PaymentService {
     private final Random random = new Random();
 
     public String payApi() {
-        int chance = random.nextInt(10);
+        int chance = random.nextInt(20);
 
         if(chance < 5) {
             throw new RuntimeException("Payment Service error");
         }
 
-        if(chance>=5 && chance<7) {
+        if(chance>=5 && chance<10) {
             try {
                 Thread.sleep(3000);
             } catch (InterruptedException e) {
